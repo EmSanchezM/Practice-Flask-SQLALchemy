ce731164a98cad48a4e2427bf93a014f98ef901d
ElvinMoisesSanchezM <elvinmoises.sanchez@gmail.com>
2020-04-11 11:02:22 -0600

    flask-sqlalchemy

---
+++ app/models.py
@@ -0,0 +1,205 @@
+from config import db, ma
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(25))
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(255))
+    logindate = db.Column(db.DateTime)
+
+    activities = db.relationship('Activity', backref='users', lazy='dynamic')
+    orders = db.relationship('Orders', backref='users', lazy='dynamic')
+
+    def __init__(self, name, email, password, logindate):
+        self.name = name
+        self.email = email
+        self.password = password
+        self.logindate = logindate
+
+    def __repr__(self):
+        return '<User %r>' % self.name
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'email', 'password', 'logindate')
+
+class Activity(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    activity = db.Column(db.String(255))
+    acDate = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def __init__(self, activity, acDate, user_id):
+        self.activity = activity
+        self.acDate = acDate
+        self.user_id = user_id
+
+    def __repr__(self):
+        return '<Activity %r>' % self.activity
+
+class ActivitySchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'activity', 'acDate', 'user_id')
+
+class Customers(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    address = db.Column(db.String(255))
+    contact = db.Column(db.String(100))
+    type_custom = db.Column(db.String(10))
+
+    orders = db.relationship('Orders', backref='customers', lazy='dynamic')
+
+    def __init__(self, name, address, contact, type_custom):
+        self.name = name
+        self.address = address
+        self.contact = contact
+        self.type_custom = type_custom
+
+    def __repr__(self):
+        return '<Customer %r>' % self.name
+
+class CustomerSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'address', 'contact', 'type_custom')
+
+class Orders(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    order_date = db.Column(db.DateTime)
+    pay_date = db.Column(db.DateTime)
+    paymentMode = db.Column(db.Integer, db.ForeignKey('payments.id'))
+    comment = db.Column(db.String(255))
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def __init__(self, order_date, pay_date, paymentMode, comment, customer_id, user_id):
+        self.order_date = order_date
+        self.pay_date = pay_date
+        self.paymentMode = paymentMode
+        self.comment = comment
+        self.customer_id = customer_id
+        self.user_id = user_id
+
+class OrderSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'order_date', 'pay_date', 'paymentMode', 'comment', 'customer_id','user_id')
+
+class Payments(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    pay_mode = db.Column(db.String(25))
+    orders = db.relationship('Orders', backref='payments', lazy='dynamic')
+
+    def __init__(self, pay_mode):
+        self.pay_mode = pay_mode
+    
+    def __repr__(self):
+        return '<Payments %r>' % self.pay_mode
+
+class PaymentSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'pay_mode')
+
+class PaymentDetail(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    quantity_ordered = db.Column(db.Integer)
+    price = db.Column(db.Float())
+    order_id = db.Column(db.Integer, db.ForeignKey('orders.id'))
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'))
+
+    def __init__(self, quantity_ordered, price, order_id, product_id):
+        self.quantity_ordered = quantity_ordered
+        self.price = price
+        self.order_id = order_id
+        self.product_id = product_id
+
+class PaymentDetailSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'quantity_ordered', 'price', 'order_id','product_id')
+
+class Products(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(45))
+    unit = db.Column(db.Integer)
+    description = db.Column(db.String(255))
+    provider_id = db.Column(db.Integer, db.ForeignKey('providers.id'))
+    paymentdetails = db.relationship('PaymentDetail', backref='products', lazy='dynamic')
+    inventories = db.relationship('Inventory', backref='products', lazy='dynamic')
+
+    def __init__(self, name, unit, description, provider_id):
+        self.name = name
+        self.unit = unit
+        self.description = description
+        self.provider_id = provider_id
+    
+    def __repr__(self):
+        return '<Product %r>' % self.name
+
+class ProductSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'unit', 'description', 'provider_id')
+
+class Providers(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    prov_license = db.Column(db.String(100))
+    address = db.Column(db.String(255))
+    products = db.relationship('Products', backref='providers', lazy='dynamic')
+
+    def __init__(self, name, prov_license, address):
+        self.name = name
+        self.prov_license = prov_license
+        self.address = address
+    
+    def __repr__(self):
+        return '<Provider %r>' % self.name
+
+class ProviderSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'prov_license', 'address')
+
+class Inventory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    cost = db.Column(db.Float())
+    price = db.Column(db.Float())
+    unit = db.Column(db.Integer)
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'))
+
+    def __init__(self, cost, price, unit, product_id):
+        self.cost = cost
+        self.price = price
+        self.unit = unit
+        self.product_id = product_id
+
+class InventorySchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'cost', 'price', 'unit', 'product_id')
+
+#Instancias
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+activity_schema = ActivitySchema()
+activities_schema = ActivitySchema(many=True)
+
+customer_Schema = CustomerSchema()
+customers_Schema = CustomerSchema(many=True)
+
+Order_Schema = OrderSchema()
+Orders_Schema = OrderSchema(many=True)
+
+payment_Schema = PaymentSchema()
+payments_Schema = PaymentSchema(many=True)
+
+paymentDetail_Schema = PaymentDetailSchema()
+paymentDetails_Schema = PaymentDetailSchema(many=True)
+
+product_Schema = ProductSchema()
+products_Schema = ProductSchema(many=True)
+
+provider_Schema = ProviderSchema()
+providers_Schema = ProviderSchema(many=True)
+
+inventory_Schema = InventorySchema()
+inventories_Schema = InventorySchema(many=True)
+
+db.create_all()
---
+++ app/routes.py
@@ -0,0 +1,453 @@
+from flask import request, jsonify
+from datetime import datetime
+from models import *
+from config import app 
+
+@app.route('/users', methods=['POST'])
+def addUser():
+    name = request.json['name']
+    email = request.json['email']
+    password = request.json['password']
+    logindate = datetime.today()
+
+    new_user = Users(name, email, password, logindate)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return user_schema.jsonify(new_user)
+
+@app.route('/users', methods=['GET'])
+def getUsers():
+    all_users = Users.query.all()
+    result = users_schema.dump(all_users)
+
+    return jsonify(result)
+
+@app.route('/users/<id>', methods=['GET'])
+def getUser(id):
+    user = Users.query.get(id)
+    return user_schema.jsonify(user)
+
+@app.route('/users/<id>', methods=['PUT'])
+def update_user(id):
+    user = Users.query.get(id)
+
+    name = request.json['name']
+    email = request.json['email']
+    password = request.json['password']
+    logindate = datetime.today()
+
+    user.name = name
+    user.email = email 
+    user.password = password
+    user.logindate = logindate
+
+    db.session.commit()
+
+    return user_schema.jsonify(user)
+
+@app.route('/users/<id>', methods=['DELETE'])
+def delete_user(id):
+    user = Users.query.get(id)
+
+    db.session.delete(user)
+    db.session.commit()
+
+    return user_schema.jsonify(user)
+
+@app.route('/activities', methods=['POST'])
+def addActivity():
+    activity = request.json['activity']
+    acDate = datetime.today()
+    user_id = request.json['user_id']
+
+    new_activity = Activity(activity, acDate, user_id)
+    db.session.add(new_activity)
+    db.session.commit()
+
+    return activity_schema.jsonify(new_activity)
+
+@app.route('/activities', methods=['GET'])
+def getActivities():
+    all_activities = Activity.query.all()
+    result = activities_schema.dump(all_activities)
+    return jsonify(result)
+
+@app.route('/activities/<id>', methods=['GET'])
+def getActivity(id):
+    activity = Activity.query.get(id)
+    return activity_schema.jsonify(activity)
+
+@app.route('/activities/<id>', methods=['PUT'])
+def update_activity(id):
+    activity = Activity.query.get(id)
+
+    activity = request.json['activity']
+    acDate = datetime.today()
+    user_id = request.json['user_id']
+
+    activity.activity = activity
+    activity.acDate = acDate
+    activity.user_id = user_id
+
+    db.session.commit()
+
+    return activity_schema.jsonify(activity)
+
+@app.route('/activities/<id>', methods=['DELETE'])
+def delete_activity(id):
+    activity = Activity.query.get(id)
+    db.session.delete(activity)
+    db.session.commit()
+
+    return activity_schema.jsonify(activity)
+
+@app.route('/customers', methods=['POST'])
+def addCustomer():
+    name = request.json['name']
+    address = request.json['address']
+    contact = request.json['contact']
+    type_custom = request.json['type_custom']
+
+    new_customer = Customers(name, address, contact, type_custom)
+
+    db.session.add(new_customer)
+    db.session.commit()
+
+    return customer_Schema.jsonify(new_customer)
+
+@app.route('/customers', methods=['GET'])
+def getCustomers():
+    all_customers = Customers.query.all()
+    result = customers_Schema.dump(all_customers)
+    return jsonify(result)
+
+@app.route('/customers/<id>', methods=['GET'])
+def getCustomer(id):
+    customer = Customers.query.get(id)
+    return customer_Schema.jsonify(customer)
+
+@app.route('/customers/<id>', methods=['PUT'])
+def update_customer(id):
+    customer = Customers.query.get(id)
+
+    name = request.json['name']
+    address = request.json['address']
+    contact = request.json['contact']
+    type_custom = request.json['type_custom']
+
+    customer.name = name
+    customer.address = address
+    customer.contact = contact
+    customer.type_custom = type_custom
+
+    db.session.commit()
+
+    return customer_Schema.jsonify(customer)
+
+@app.route('/customers/<id>', methods=['DELETE'])
+def delete_customer(id):
+    customer = Customers.query.get(id)
+    db.session.delete(customer)
+    db.session.commit()
+
+    return customer_Schema.jsonify(customer)
+
+@app.route('/providers', methods=['POST'])
+def addProvider():
+    name = request.json['name']
+    prov_license = request.json['prov_license']
+    address = request.json['address']
+
+    new_provider = Providers(name, prov_license, address)
+    db.session.add(new_provider)
+    db.session.commit()
+
+    return provider_Schema.jsonify(new_provider)
+    
+@app.route('/providers', methods=['GET'])
+def getProviders():
+    all_providers = Providers.query.all()
+    result = providers_Schema.dump(all_providers)
+    return jsonify(result)
+
+@app.route('/providers/<id>', methods=['GET'])
+def getProvider(id):
+    provider = Providers.query.get(id)
+    return provider_Schema.jsonify(provider)
+
+@app.route('/providers/<id>')
+def update_provider(id):
+    provider = Providers.query.get(id)
+
+    name = request.json['name']
+    prov_license = request.json['prov_license']
+    address = request.json['address']
+
+    provider.name = name
+    provider.prov_license = prov_license
+    provider.address = address
+
+    db.session.commit()
+
+    return provider_Schema.jsonify(provider)
+
+@app.route('/providers/<id>')
+def delete_provider(id):
+    provider = Providers.query.get(id)
+    db.session.delete(provider)
+    db.session.commit()
+
+    return provider_Schema.jsonify(provider)
+
+@app.route('/products', methods=['POST'])
+def addProducts():
+    name = request.json['name']
+    unit = request.json['unit']
+    description = request.json['description']
+    provider_id = request.json['provider_id']
+
+    new_product = Products(name, unit, description, provider_id)
+    db.session.add(new_product)
+    db.session.commit()
+
+    return product_Schema.jsonify(new_product)
+    
+@app.route('/products', methods=['GET'])
+def getProducts():
+    all_products = Products.query.all()
+    result = products_Schema.dump(all_products)
+    return jsonify(result)
+
+@app.route('/products/<id>', methods=['GET'])
+def getProduct(id):
+    product = Products.query.get(id)
+    return product_Schema.jsonify(product)
+
+@app.route('/products/<id>', methods=['PUT'])
+def update_product(id):
+    product = Products.query.get(id)
+    
+    name = request.json['name']
+    unit = request.json['unit']
+    description = request.json['description']
+    provider_id = request.json['provider_id']
+
+    product.name = name
+    product.unit = unit
+    product.description = description
+    product.provider_id = provider_id
+
+    db.session.commit()
+
+    return product_Schema.jsonify(product)
+
+@app.route('/products/<id>', methods=['DELETE'])
+def delete_product(id):
+    product = Products.query.get(id)
+    db.session.delete(product)
+    db.session.commit()
+
+    return product_Schema.jsonify(product)
+
+@app.route('/orders', methods=['POST'])
+def addOrder():
+    order_date = datetime.today()
+    pay_date = datetime.today()
+    paymentMode = request.json['paymentMode']
+    comment = request.json['comment']
+    customer_id = request.json['customer_id']
+    user_id = request.json['user_id']
+
+    new_Order = Orders(order_date, pay_date, paymentMode, comment, customer_id, user_id)
+    db.session.add(new_Order)
+    db.session.commit()
+
+    return Order_Schema.jsonify(new_Order)
+
+@app.route('/orders', methods=['GET'])
+def getOrders():
+    all_orders = Orders.query.all()
+    result = Orders_Schema.dump(all_orders)
+    return jsonify(result)
+
+@app.route('/orders/<id>', methods=['GET'])
+def getOrder(id):
+    order = Orders.query.get(id)
+    return Order_Schema.jsonify(order)
+
+@app.route('/orders/<id>', methods=['PUT'])
+def update_order(id):
+    order = Orders.query.get(id)
+
+    order_date = datetime.today()
+    pay_date = datetime.today()
+    paymentMode = request.json['paymentMode']
+    comment = request.json['comment']
+    customer_id = request.json['customer_id']
+    user_id = request.json['user_id']
+
+    order.order_date = order_date
+    order.pay_date = pay_date
+    order.paymentMode = paymentMode
+    order.commet = comment
+    order.customer_id = customer_id
+    order.user_id = user_id
+
+    db.session.commit()
+
+    return Order_Schema.jsonify(order)
+
+@app.route('/orders/<id>', methods=['DELETE'])
+def delete_order(id):
+    order = Orders.query.get(id)
+    db.session.delete(order)
+    db.session.commit()
+    return Order_Schema.jsonify(order)
+
+@app.route('/payments', methods=['POST'])
+def addPayment():
+    pay_mode = request.json['pay_mode']
+
+    new_payment = Payments(pay_mode)
+    db.session.add(new_payment)
+    db.session.commit()
+
+    return payment_Schema.jsonify(new_payment)
+
+@app.route('/payments', methods=['GET'])
+def getPayments():
+    all_payments = Payments.query.all()
+    result = payments_Schema.dump(all_payments)
+    return jsonify(result)
+
+@app.route('/payments/<id>', methods=['GET'])
+def getPayment(id):
+    payment = Payments.query.get(id)
+    return payment_Schema.jsonify(payment)
+
+@app.route('/payments/<id>', methods=['PUT'])
+def update_payment(id):
+    payment = Payments.query.get(id)
+
+    pay_mode = request.json['pay_mode']
+
+    payment.pay_mode = pay_mode
+
+    db.session.commit()
+
+    return payment_Schema.jsonify(payment)
+
+@app.route('/payments/<id>', methods=['DELETE'])
+def delete_payments(id):
+    payment = Payments.query.get(id)
+    db.session.delete(payment)
+    db.session.commit()
+
+    return payment_Schema.jsonify(payment)
+
+@app.route('/payDetails', methods=['POST'])
+def addPayDetails():
+    quantity_ordered = request.json['quantity_ordered']
+    price = request.json['price']
+    order_id = request.json['order_id']
+    product_id = request.json['product_id']
+
+    new_payDetail = PaymentDetail(quantity_ordered, price, order_id, product_id)
+    db.session.add(new_payDetail)
+    db.session.commit()
+
+    return paymentDetail_Schema.jsonify(new_payDetail)
+
+@app.route('/payDetails', methods=['GET'])
+def getPayDetails():
+    all_payDetails = PaymentDetail.query.all()
+    result = paymentDetails_Schema.dump(all_payDetails)
+    return jsonify(result)
+
+@app.route('/payDetails/<id>', methods=['GET'])
+def getPayDetail(id):
+    payDetail = PaymentDetail.query.get(id)
+    return paymentDetail_Schema.jsonify(payDetail)
+
+@app.route('/payDetails/<id>', methods=['PUT'])
+def update_payDetails(id):
+    payDetail = PaymentDetail.query.get(id)
+
+    quantity_ordered = request.json['quantity_ordered']
+    price = request.json['price']
+    order_id = request.json['order_id']
+    product_id = request.json['product_id']
+
+    payDetail.quantity_ordered = quantity_ordered
+    payDetail.price = price
+    payDetail.order_id = order_id
+    payDetail.product_id = product_id
+
+    db.session.commit()
+
+    return paymentDetail_Schema.jsonify(payDetail)
+
+@app.route('/payDetails/<id>', methods=['DELETE'])
+def delete_payDetails(id):
+    payDetail = PaymentDetail.query.get(id)
+    db.session.delete(payDetail)
+    db.session.commit()
+
+    return paymentDetail_Schema.jsonify(payDetail)
+
+@app.route('/inventory', methods=['POST'])
+def addInventory():
+    cost = request.json['cost']
+    price = request.json['price']
+    unit = request.json['unit']
+    product_id = request.json['product_id']
+
+    new_inventory = Inventory(cost, price, unit, product_id)
+
+    db.session.add(new_inventory)
+    db.session.commit()
+
+    return inventory_Schema.jsonify(new_inventory)
+
+@app.route('/inventory', methods=['GET'])
+def getInventories():
+    all_inventories = Inventory.query.all()
+    result = inventories_Schema.dump(all_inventories)
+    return jsonify(result) 
+
+@app.route('/inventory/<id>', methods=['GET'])
+def getInventory(id):
+    inventory = Inventory.query.get(id)
+    return inventory_Schema.jsonify(inventory)
+
+@app.route('/inventory/<id>', methods=['PUT'])
+def update_inventory(id):
+    inventory = Inventory.query.get(id)
+
+    cost = request.json['cost']
+    price = request.json['price']
+    unit = request.json['unit']
+    product_id = request.json['product_id']
+
+    inventory.cost = cost
+    inventory.price = price
+    inventory.unit = unit
+    inventory.product_id = product_id
+
+    db.session.commit()
+
+    return inventory_Schema.jsonify(inventory) 
+
+@app.route('/inventory/<id>', methods=['DELETE'])
+def delete_inventory(id):
+    inventory = Inventory.query.get(id)
+    db.session.delete(inventory)
+    ds.session.commit()
+
+    return inventory_Schema.jsonify(inventory)
+
+@app.route('/')
+def index():
+    return jsonify({'message': 'Welcome to API Pharmacy'})
